770f870b4f46bc380f30633337e0acd303b47bc0
Katastrophenmagnet <sarah.kocher12@gmail.com>
2024-09-24 00:18:13 +0200

    added model and view for expenses

--- SpentSmart/Models/Expense.cs
+++ SpentSmart/Models/Expense.cs
@@ -1,6 +1,13 @@
-﻿namespace SpentSmart.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpentSmart.Models
 {
     public class Expense
     {
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+        
+        [Required]
+        public string? Description { get; set; }
     }
 }
