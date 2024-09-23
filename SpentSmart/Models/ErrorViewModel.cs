eb49763c50b3364fb737a9bb75ec8b1a87d8d0ea
Katastrophenmagnet <Katastrophenmagnet@DESKTOP-5IKL9OB>
2024-09-23 23:59:25 +0200

    Projektdateien hinzufügen.

---
+++ SpentSmart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SpentSmart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
